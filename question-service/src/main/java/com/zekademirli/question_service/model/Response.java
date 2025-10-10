ec1db106ed1c4b9122a0d634f0ccde3f2a1bed2d
zakadamirli <z3kkadamirli@gmail.com>
2025-10-10 09:21:18 +0400

    feat: use lombok

--- question-service/src/main/java/com/zekademirli/question_service/model/Response.java
+++ question-service/src/main/java/com/zekademirli/question_service/model/Response.java
@@ -1,22 +1,12 @@
 package com.zekademirli.question_service.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Response {
     private Integer id;
     private String response;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
 }
